7b22aa8ec3886a46c2b5809487654c6c5c87d101
Adithya Anand Gondkar <85690929+adithya-ag@users.noreply.github.com>
2026-08-03 22:07:41 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j){
+            char current = s.charAt(i);
+            char end = s.charAt(j);
+
+            if(!Character.isLetterOrDigit(current)){
+                i++;
+                continue;
+            }
+
+            if(!Character.isLetterOrDigit(end)){
+                j--;
+                continue;
+            }
+
+            if(Character.toLowerCase(current) != Character.toLowerCase(end)){
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
